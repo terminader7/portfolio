67cc7dd756cc30f95ed2d19c8f5bbeacb80e08f2
Nader Ebrahim <nebrahim2424@gmail.com>
2024-01-22 23:27:07 -0800

    small tweak in the theme file

--- config/theme.ts
+++ config/theme.ts
@@ -1,113 +1,106 @@
 import {
-    Breakpoints,
-    darken,
-    PaletteMode,
-    ThemeOptions,
-    PaletteOptions,
-  } from "@mui/material";
-  
-  export const breakpoints: { values: Breakpoints["values"] } = {
-    values: {
-      xs: 0,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
+  Breakpoints,
+  darken,
+  PaletteMode,
+  ThemeOptions,
+  PaletteOptions,
+} from "@mui/material";
+
+export const breakpoints: { values: Breakpoints["values"] } = {
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
+  },
+};
+
+export const getColorOptions = (mode: "light" | "dark"): PaletteOptions => {
+  const light = {
+    primary: {
+      light: "#FB9B4C",
+      main: "#FA8320",
+      dark: "#C86819",
     },
+    secondary: {
+      light: "#4CABFB",
+      main: "#2097fa",
+      dark: "#1978C8",
+    },
+    tertiary: {
+      main: "#002F95",
+    },
+    background: {
+      // This is more of a standard grey
+      // default: "#3F598E",
+      default: "#4F7390",
+      paper: "#FFFFFF",
+    },
+    divider: "rgba(0, 0, 0, 0.12)",
   };
-  
-  export const getColorOptions = (mode: "light" | "dark"): PaletteOptions => {
-    const light = {
-      primary: {
-        light: "#FB9B4C",
-        main: "#FA8320",
-        dark: "#C86819",
-      },
-      secondary: {
-        light: "#4CABFB",
-        main: "#2097fa",
-        dark: "#1978C8",
-      },
-      tertiary: {
-        main: "#002F95",
-      },
-      error: {
-        main: "#D32F2F",
-      },
-      warning: {
-        main: "#ED6C02",
-      },
-      background: {
-        // This is more of a standard grey
-        // default: "#3F598E",
-        default: "#4F7390",
-        paper: "#FFFFFF",
-      },
-      divider: "rgba(0, 0, 0, 0.12)",
-    };
-  
-    const dark = {
-      primary: light.primary,
-      secondary: light.secondary,
-      tertiary: light.tertiary,
-    };
-  
-    return mode === "dark" ? dark : light;
+
+  const dark = {
+    primary: light.primary,
+    secondary: light.secondary,
+    tertiary: light.tertiary,
   };
-  
-  export const getThemeOptions = (
-    mode: PaletteMode,
-    primaryColor: string = "",
-    secondaryColor: string = ""
-  ): ThemeOptions => ({
-    palette: {
-      mode,
-      ...getColorOptions(mode),
-      ...(Boolean(primaryColor)
-        ? {
-            primary: {
-              main: primaryColor,
-            },
-            tertiary: {
-              main: darken(primaryColor, 0.25),
-            },
-          }
-        : {}),
-      ...(Boolean(secondaryColor)
-        ? {
-            secondary: {
-              main: secondaryColor,
-            },
-          }
-        : {}),
-    },
-    typography: {
-      fontFamily: "Avenir, sans-serif",
-      fontSize: 14,
-    },
-    components: {
-      MuiLink: {
-        styleOverrides: {
-          root: {
-            color: "currentColor",
-            fontFamily: "Avenir, sans-serif",
+
+  return mode === "dark" ? dark : light;
+};
+
+export const getThemeOptions = (
+  mode: PaletteMode,
+  primaryColor: string = "",
+  secondaryColor: string = ""
+): ThemeOptions => ({
+  palette: {
+    mode,
+    ...getColorOptions(mode),
+    ...(Boolean(primaryColor)
+      ? {
+          primary: {
+            main: primaryColor,
           },
-        },
-      },
-      MuiIcon: {
-        styleOverrides: {
-          root: {
-            color: "currentColor",
-            fill: "currentColor",
+          tertiary: {
+            main: darken(primaryColor, 0.25),
           },
+        }
+      : {}),
+    ...(Boolean(secondaryColor)
+      ? {
+          secondary: {
+            main: secondaryColor,
+          },
+        }
+      : {}),
+  },
+  typography: {
+    fontFamily: "Avenir, sans-serif",
+    fontSize: 14,
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          color: "currentColor",
+          fontFamily: "Avenir, sans-serif",
         },
       },
     },
-    breakpoints,
-    shape: {
-      borderRadius: 10,
+    MuiIcon: {
+      styleOverrides: {
+        root: {
+          color: "currentColor",
+          fill: "currentColor",
+        },
+      },
     },
-  });
-  
-  export default getThemeOptions;
-  
+  },
+  breakpoints,
+  shape: {
+    borderRadius: 10,
+  },
+});
+
+export default getThemeOptions;

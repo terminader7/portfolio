919528917bab9dcc8aedd782423594ee87b85893
Nader Ebrahim <nebrahim2424@gmail.com>
2024-01-22 23:15:47 -0800

    set up theme config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import '../styles/globals.css'
+import { CssBaseline, PaletteMode } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import getThemeOptions from "../config/theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { mode, primaryColor, secondaryColor } = pageProps;
+
+  const getPreferredTheme = (): PaletteMode => {
+    if (typeof window !== "undefined" && window.matchMedia) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "light";
+      }
+    }
+    return "light";
+  };
+
+  const theme = useMemo(
+    () =>
+      createTheme(
+        getThemeOptions(getPreferredTheme(), primaryColor, secondaryColor)
+      ),
+    [primaryColor, secondaryColor]
+  );
+  return( 
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+  <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp

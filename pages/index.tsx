5cafd7b34160c03b41dd1fa65083a73f9ace9d6e
Nader Ebrahim <nebrahim2424@gmail.com>
2024-01-23 11:01:35 -0800

    uploaded picture of myself, establishing skeleton right now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Box, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,9 +10,10 @@ export default function Home() {
         <meta name="description" content="Nader's Portfolio" />
         <link rel="icon" href="/images/favicon.svg" />
       </Head>
-      <Typography variant="h1" sx={{ color: "green.main" }}>
-        Naders Portfolio
+      <Typography variant="h4" sx={{ color: "green.main" }}>
+        {`Hey! Thanks for stopping by, I'm Nader.`}
       </Typography>
+      <Image src="/images/picOfMe.jpg" alt="Nader" width={600} height={600} />
     </Box>
   );
 }

28c37ee2c709068cf0f95c0a4af4d585e319be95
Nader Ebrahim <nebrahim2424@gmail.com>
2024-02-12 09:25:25 -0800

    small tweaks to query, still need to work on it

--- features/queries.tsx
+++ features/queries.tsx
@@ -1,11 +1,9 @@
 export const getRandomQuote = async () => {
   try {
-    const response = await fetch("https://api.quotable.io");
-
+    const response = await fetch("https://api.quotable.io/random");
     if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     const data = await response.json();
     return data;
   } catch (error) {

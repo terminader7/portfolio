bd2ff748c77997e7275b81524900e3fa5c3a6de9
Nader Ebrahim <nebrahim2424@gmail.com>
2024-02-12 08:25:01 -0800

    Got rid of line breaks in the message container

--- components/MessageContainer.tsx
+++ components/MessageContainer.tsx
@@ -1,34 +1,29 @@
 import { Box } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+
 const MessageContainer = ({ messages }) => {
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
-      {messages?.map((message, index) => {
-        const segments = message.message.split(/\.(?=\s)|!(?=\s)/);
-
-        return segments.map((segment, segmentIndex) => (
-          <Paper
-            key={`${index}-${segmentIndex}`}
-            elevation={3}
-            sx={{
-              width: "fit-content",
-              padding: ".5rem",
-              margin: ".25rem",
-              backgroundColor: message.isUser
-                ? "common.white"
-                : "primary.green",
-              color: message.isUser ? "common.black" : "common.white",
-              borderRadius: "10px",
-              borderBottomRightRadius: message.isUser ? "0px" : "10px",
-              borderBottomLeftRadius: message.isUser ? "10px" : "0px",
-              alignSelf: message.isUser ? "flex-end" : "flex-start",
-            }}
-          >
-            <Typography variant="body1">{segment}</Typography>
-          </Paper>
-        ));
-      })}
+      {messages?.map((message, index) => (
+        <Paper
+          key={index}
+          elevation={3}
+          sx={{
+            width: "fit-content",
+            padding: ".5rem",
+            margin: ".25rem",
+            backgroundColor: message.isUser ? "common.white" : "primary.green",
+            color: message.isUser ? "common.black" : "common.white",
+            borderRadius: "10px",
+            borderBottomRightRadius: message.isUser ? "0px" : "10px",
+            borderBottomLeftRadius: message.isUser ? "10px" : "0px",
+            alignSelf: message.isUser ? "flex-end" : "flex-start",
+          }}
+        >
+          <Typography variant="body1">{message.message}</Typography>
+        </Paper>
+      ))}
     </Box>
   );
 };
